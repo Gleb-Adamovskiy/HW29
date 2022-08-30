a5e6aecc2b1580cbc4da0a949debe4bdb8082f5d
Gleb-Adamovskiy <54521870+Gleb-Adamovskiy@users.noreply.github.com>
2022-08-30 09:26:27 +0700

    Add files via upload

---
+++ ads/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from users.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
+
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.PositiveIntegerField()
+    description = models.TextField(max_length=1000, null=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="ads/", null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
+
+    def __str__(self):
+        return self.name
+
+
+class Selection(models.Model):
+    name = models.CharField(max_length=20)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Ad)
